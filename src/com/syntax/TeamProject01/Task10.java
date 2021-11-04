b884bbd27059dfdd8e3bbb54e71cfc342176a374
isorbon <isorbon@gmail.com>
2021-11-04 11:32:43 +0200

    Added Team Project Part 01 - Task 10

--- src/com/syntax/TeamProject01/Task10.java
+++ src/com/syntax/TeamProject01/Task10.java
@@ -6,14 +6,16 @@ public class Task10 {
 
 	public static void main(String[] args) {
 		// Write a java program to find the second largest number in the array?
-
 		Scanner inp = new Scanner(System.in);
 		int size, temp;
-		System.out.print("Please enter the Array's size: ");
-		size = inp.nextInt();
-
+		
+		do {
+			System.out.print("Please enter the Array's size (greater than 0): ");
+			size = inp.nextInt();
+		} while (size <= 0);
+		
 		int[] num = new int[size];
-
+		
 		System.out.println("Please enter the Array's numbers:");
 		for (int i = 0; i < size; i++) {
 			num[i] = inp.nextInt();
